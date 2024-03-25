fa7ef307104db9981de1c147fb2f447100593892
Osiloke Emoekpere <osi@progwebtech.com>
2024-03-25 19:26:50 +0100

    rename deps and project name

--- badger_indexer.go
+++ badger_indexer.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/blevesearch/bleve/v2"
 	"github.com/blevesearch/bleve/v2/index/upsidedown"
-	"github.com/osiloke/gostore-contrib/indexer/badger"
-	_ "github.com/osiloke/gostore-contrib/indexer/badger"
+	"github.com/osiloke/gostore-indexer/badger"
+	_ "github.com/osiloke/gostore-indexer/badger"
 
 	"github.com/blevesearch/bleve/v2/mapping"
 )
